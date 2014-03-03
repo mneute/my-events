e8e418afe4746aa727dff636403d312e943591be
mneute <morgan.neute@gmail.com>
2014-03-03 15:27:04 +0100

    MaJ du nom de fonction + utilisation de l'annotation @Template

--- src/MyEvents/MainBundle/Controller/DefaultController.php
+++ src/MyEvents/MainBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace MyEvents\MainBundle\Controller;
 
 use MyEvents\MainBundle\Entity\Utilisateur;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\SecurityContext;
 
@@ -24,40 +25,38 @@ class DefaultController extends Controller {
 
 	/**
 	 * @Route("/", name="accueil", options={"expose"=true})
+	 * @Template("MainBundle:Default:index.html.twig")
 	 */
-	public function listeDemandeAmisAction() {
+	public function accueilAction() {
 
-		$toListeDemandeAmis = array();
-		$listeAmis = array();
+		$toListeDemandeAmis = [];
+		$listeAmis = [];
 		if ($this->oUtilisateurConnecte) {
 			$toListeDemandeAmis = $this->oUtilisateurConnecte->getListesDemandeAmiEnAttente($this->oUtilisateurConnecte);
 			$listeAmis = $this->oUtilisateurConnecte->getListeAmisValides($this->oUtilisateurConnecte);
 		}
 
-		$tListeDemandeAmis = array();
+		$tListeDemandeAmis = [];
 		foreach ($toListeDemandeAmis as $key => $oAmi) {
-			$tListeDemandeAmis[] = array(
-				'idDemandeAmi' => $key,
-				'idUtilisateur' => $oAmi->getId(),
-				'nom' => $oAmi->getNom(),
-				'prenom' => $oAmi->getPrenom(),
-				'username' => $oAmi->getUsername()
-			);
+			$tListeDemandeAmis[] = [
+			   'idDemandeAmi' => $key,
+			   'idUtilisateur' => $oAmi->getId(),
+			   'nom' => $oAmi->getNom(),
+			   'prenom' => $oAmi->getPrenom(),
+			   'username' => $oAmi->getUsername()
+			];
 		}
 
-		$amis = array();
+		$amis = [];
 		foreach ($listeAmis as $ami) {
-			$amis[] = array(
-				'id' => $ami->getId(),
-				'nom' => $ami->getNom(),
-				'prenom' => $ami->getPrenom(),
-				'username' => $ami->getUsername()
-			);
+			$amis[] = [
+			   'id' => $ami->getId(),
+			   'nom' => $ami->getNom(),
+			   'prenom' => $ami->getPrenom(),
+			   'username' => $ami->getUsername()
+			];
 		}
 
-		return $this->render('MainBundle:Default:index.html.twig', array(
-					'listeDemandeAmis' => $tListeDemandeAmis,
-					'listeAmis' => $amis));
+		return ['listeDemandeAmis' => $tListeDemandeAmis, 'listeAmis' => $amis];
 	}
-
 }
